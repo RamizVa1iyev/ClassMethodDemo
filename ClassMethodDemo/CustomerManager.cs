829ff008827d1155397b11cb0e940320a465efc5
Ramiz Vəliyev <ramiz.valiyev.587@teams.dia.edu.az>
2021-01-21 19:05:44 +0400

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        private readonly List<Customer> _customers;
+
+        public CustomerManager()
+        {
+            _customers = new List<Customer>()
+            {
+                new Customer(){Id = 1,FirstName = "Ramiz",LastName = "Valiyev"},
+                new Customer(){Id = 2,FirstName = "Asim",LastName = "Elizade"},
+                new Customer(){Id = 3,FirstName = "Vuqar",LastName = "Kazimov"}
+            };
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _customers;
+        }
+
+        public List<Customer> GetByName(string key)
+        {
+            return _customers.Where(c => c.FirstName.ToLower().Contains(key.ToLower())).ToList();
+        }
+
+        public void Add(Customer customer)
+        {
+            _customers.Add(customer);
+            Console.WriteLine("Added");
+        }
+
+        public void Update(Customer customer)
+        {
+            var updatedCustomer = _customers.FirstOrDefault(c=>c.Id==customer.Id);
+            _customers[_customers.IndexOf(updatedCustomer)] = customer;
+            Console.WriteLine("Updated");
+        }
+
+        public void Delete(Customer customer)
+        {
+            var deletedCustomer = _customers.FirstOrDefault(c => c.Id == customer.Id);
+            _customers.Remove(deletedCustomer);
+            Console.WriteLine("Deleted");
+        }
+
+        public void Clear()
+        {
+            _customers.Clear();
+            Console.WriteLine("Deleted all customers");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = new CustomerManager();
+            customerManager.Add(new Customer(){Id = 4,FirstName = "Afet",LastName = "Agayeva"});
+            customerManager.Update(new Customer(){Id = 4,FirstName = "Afet",LastName = "Valiyeva"});
+            customerManager.Delete(new Customer(){Id = 4});
+            //customerManager.Clear();
+            foreach (var customer in customerManager.GetAll())
+            {
+                Console.WriteLine("{0} {1} {2}",customer.Id,customer.FirstName,customer.LastName);
+            }
+
+            var customers = customerManager.GetByName("VUQar");
+            Console.WriteLine("{0} {1} {2}", customers[0].Id, customers[0].FirstName, customers[0].LastName);
+        }
+    }
+}
